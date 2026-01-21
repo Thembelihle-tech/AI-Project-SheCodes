989e45020a6859cee2b8e9f506623aca0bc3e1ec
ThembelihleNkosi1 <nkosit@imentorufoundation.org>
2026-01-21 15:10:47 +0200

    Update ai-project.js

--- ai-project.js
+++ ai-project.js
@@ -1,1 +1,8 @@
-alert("test");
+function generatePoem(event){
+    event.preventDefault();
+
+    alert("Generate poem");
+}
+
+let poemFormElement = document.quesrySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
