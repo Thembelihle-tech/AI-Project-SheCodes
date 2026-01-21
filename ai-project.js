20746501c35809d66f0e9569f278812ee1c87adf
ThembelihleNkosi1 <nkosit@imentorufoundation.org>
2026-01-21 15:26:31 +0200

    Added basic form handling and added typewriter

--- ai-project.js
+++ ai-project.js
@@ -2,6 +2,13 @@ function generatePoem(event){
     event.preventDefault();
 
     let poemElement = document.querySelector("#poem");
+
+    new Typewriter('#poem', {
+  strings: "Come, And Be My Bab",
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
     poemElement.innerHTML = "Come, And Be My Baby";
 }
 
